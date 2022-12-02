60e0bbbfd9e531b792fbbaf8fc8a33c6bb5760b6
Feynman <2901954062@qq.com>
2022-12-02 11:00:56 +0800

    refactor: vue3基础语法使用规整，后续其他使用demo 文件夹整理📦📦📦

--- vite_learn/src/App.vue
+++ vite_learn/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import TemplateSyntax from "./components/template_syntax.vue";
+import TemplateSyntax from "./components/basic/template_syntax.vue";
 </script>
 
 <template>
