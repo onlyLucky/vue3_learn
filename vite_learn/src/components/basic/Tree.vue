538b34e1fbd8bd11f1fb6789d02f6554b5cd1b53
Feynman <2901954062@qq.com>
2022-12-08 18:08:35 +0800

    workflow: vite 运行添加ip运行，新增递归组件该名称的依赖，另一种方法修改递归组件名称添加

--- vite_learn/src/components/basic/Tree.vue
+++ vite_learn/src/components/basic/Tree.vue
@@ -2,15 +2,21 @@
  * @Author: fg
  * @Date: 2022-12-08 17:20:14
  * @LastEditors: fg
- * @LastEditTime: 2022-12-08 17:45:11
+ * @LastEditTime: 2022-12-08 18:06:11
  * @Description: 递归组件
 -->
 <template>
-  <div class="tree" v-for="(item, index) in data" :key="index">
+  <div
+    class="tree"
+    @click.stop="clickItem(item)"
+    v-for="(item, index) in data"
+    :key="index"
+  >
     <input type="checkbox" v-model="item.checked" /><span>{{ item.name }}</span>
     <!-- 直接使用文件名当组件名 Tree.vue 对应递归组件的名称是Tree -->
     <!-- <Tree v-if="item?.children?.length" :data="item?.children"></Tree> -->
-    <recursion v-if="item?.children?.length" :data="item?.children"></recursion>
+    <!-- <recursion v-if="item?.children?.length" :data="item?.children"></recursion> -->
+    <TreeTree v-if="item?.children?.length" :data="item?.children"></TreeTree>
   </div>
 </template>
 <script setup lang="ts">
@@ -21,22 +27,27 @@ type TreeList = {
   children?: TreeList[] | [];
 };
 
+defineOptions({
+  name: "TreeTree",
+});
+
 type Props<T> = {
   data?: T[] | [];
 };
 
 defineProps<Props<TreeList>>();
 
+// 递归组件点击事件要添加冒泡阻止事件
 const clickItem = (item: TreeList) => {
   console.log(item);
 };
 </script>
-<script lang="ts">
+<!-- <script lang="ts">
 // 如果不想直接使用组件名称，子组件增加一个script 定义组件名称为了 递归用
 export default {
   name: "recursion",
 };
-</script>
+</script> -->
 
 <style scoped lang="less">
 .tree {
--- vite_learn/vite.config.ts
+++ vite_learn/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import DefineOptions from 'unplugin-vue-define-options/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    DefineOptions(),
+    vue()
+  ],
+  server: {
+    host: '0.0.0.0',
+  }
 })
