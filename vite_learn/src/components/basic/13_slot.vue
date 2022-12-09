99f8128a18e54df496bad70c99eac87ed1466681
Feynman <2901954062@qq.com>
2022-12-09 16:18:46 +0800

    workflow: 基础语法引入添加主入口，app.vue主入口整理，插槽初始化使用

---
+++ vite_learn/src/components/basic/13_slot.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: fg
+ * @Date: 2022-12-09 15:40:17
+ * @LastEditors: fg
+ * @LastEditTime: 2022-12-09 15:51:27
+ * @Description: 插槽
+-->
+
+<template>
+  <h3>插槽slot</h3>
+  <ul>
+    <li>
+      <h4>匿名插槽</h4>
+      <div>
+        <slot></slot>
+      </div>
+    </li>
+  </ul>
+</template>
+<script setup lang="ts"></script>
+<style></style>
