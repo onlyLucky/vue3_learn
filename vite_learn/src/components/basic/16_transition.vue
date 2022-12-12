6303fa86d3ed2a200711a1dad73f94678c742ec2
Feynman <2901954062@qq.com>
2022-12-12 14:42:56 +0800

    feat: vue3 transition init

---
+++ vite_learn/src/components/basic/16_transition.vue
@@ -0,0 +1,11 @@
+<!--
+ * @Author: fg
+ * @Date: 2022-12-12 14:40:03
+ * @LastEditors: fg
+ * @LastEditTime: 2022-12-12 14:41:37
+ * @Description: transition
+-->
+
+<template></template>
+<script setup lang="ts"></script>
+<style></style>
