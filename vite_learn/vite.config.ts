6625d7142317e8b52ad0898ce05f63ac6dfeb841
feynman <2901954062@qq.com>
2022-12-22 22:59:30 +0800

    workflow: vite 新增路径别名配置

--- vite_learn/vite.config.ts
+++ vite_learn/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import DefineOptions from 'unplugin-vue-define-options/vite'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     DefineOptions(),
     vue()
   ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"), // 设置@指向src
+    },
+  },
   server: {
     host: '0.0.0.0',
   }
