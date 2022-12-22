b725ee23fbaef0a3d2c56eb43a3ae83777f43685
feynman <2901954062@qq.com>
2022-12-22 14:10:47 +0800

    feat: vue3 实现Bus总线 ，vue原型对象属性添加

--- vite_learn/src/main.ts
+++ vite_learn/src/main.ts
@@ -2,10 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import GlobalComps from "./components/basic/11_globalComps.vue"
+import Bus from "./plugins/bus"
 
 /* const vueApp = createApp(App)
 vueApp.component('GlobalComps', GlobalComps)
 vueApp.component('GlobalHandle', GlobalComps)
 vueApp.mount('#app') */
-createApp(App).component('GlobalComps', GlobalComps).component('GlobalHandle', GlobalComps).mount('#app')
+const app = createApp(App)
+app.component('GlobalComps', GlobalComps).component('GlobalHandle', GlobalComps)
+app.mount('#app')
 // .mount('#app')要放在最后一步
+
+// vue3全局属性配置
+app.config.globalProperties.$Bus = Bus;
